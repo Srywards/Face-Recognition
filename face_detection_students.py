e1ef48147f6626f5d9e7619523ce14eb49f94e5c
Clem <clement.da-purificacao@epitech.eu>
2019-03-14 14:02:14 +0100

    GUI for input added

--- face_detection_students.py
+++ face_detection_students.py
@@ -2,14 +2,40 @@ from urllib.request import urlopen
 import numpy as np
 import cv2
 import sys
+from tkinter import *
 from PIL import Image
 
+def stocktext():
+    global string_id_student
+    global string_ip
+    global id_student
+    global ip
+    string = string_id_student.get()
+    string2 = string_ip.get()
+    id_student = string
+    ip = string2
+    root.destroy()
+
+root = Tk()
+root.title('User Infos')
+root.geometry("400x200")
+label_id = Label(root, text="Enter an id for the user (for example : 1, 2, 3)\nDo not take 1 for example if there is student.1.0.jpg\nChoose a number that is not part of the dataset folder :")
+label_id.pack()
+string_id_student = Entry(root)
+string_id_student.pack()
+string_id_student.focus_set()
+label_ip = Label(root, text="What is the ip of the camera ?")
+label_ip.pack()
+string_ip = Entry(root)
+string_ip.pack()
+b = Button(root,text='Confirm',command=stocktext)
+b.pack(side='bottom')
+root.mainloop()
+
+print("This is your ID : ", id_student)
+print("This is your IP : ", ip)
 i = 0
 frontal_face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-print("Enter an id for the user (for example : 1, 2, 3) ")
-print("Do not take 1 for example if there is student.1.0.jpg ")
-id_student = input("Choose a number that is not part of the dataset folder : ")
-ip = input("What is the ip of the camera ? ")
 url = 'http://' + str(ip) + ':8080/shot.jpg'
 
 while True:
@@ -33,5 +59,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     if i > 100:
+        print("Detection complete, now execute training_students.py")
         break
 
--- face_recognition_students.py
+++ face_recognition_students.py
@@ -3,6 +3,26 @@ import sys
 import cv2
 import numpy as np
 import os
+from tkinter import *
+
+def stockip():
+    global string_ip
+    global ip
+    string = string_ip.get()
+    ip = string
+    root.destroy()
+
+root = Tk()
+root.title('IP camera')
+root.geometry("300x100")
+label_ip = Label(root, text="What is the ip of the camera ?")
+label_ip.pack()
+string_ip = Entry(root)
+string_ip.pack()
+string_ip.focus_set()
+b = Button(root,text='Confirm',command=stockip)
+b.pack(side='bottom')
+root.mainloop()
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer/trainer.yml')
@@ -10,8 +30,7 @@ cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath);
 font = cv2.FONT_HERSHEY_SIMPLEX
 id = 0
-names = ['Unknow', 'Clement']
-ip = input("What is the ip of the camera ? ")
+names = ['Unknow', 'Hugo', 'Flavien', 'Clement', 'Jeremy']
 url = 'http://' + str(ip) + ':8080/shot.jpg'
 
 while True:
@@ -31,7 +50,7 @@ while True:
     for(x,y,w,h) in faces:
         cv2.rectangle(image_frame, (x,y), (x+w,y+h), (255,0,0), 2)
         id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-        if (confidence < 35):
+        if (confidence < 100):
             id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
         else:

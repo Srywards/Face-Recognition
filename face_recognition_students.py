bf6be0ff855c8ef3ab771266410bbbd072e3da98
Clem <clement.da-purificacao@epitech.eu>
2019-03-18 16:57:46 +0100

    DB Sqlite added in memory

--- face_recognition_students.py
+++ face_recognition_students.py
@@ -3,6 +3,8 @@ import sys
 import cv2
 import numpy as np
 import os
+import time
+import sqlite3
 from tkinter import *
 
 def stockip():
@@ -24,15 +26,18 @@ b = Button(root,text='Confirm',command=stockip)
 b.pack(side='bottom')
 root.mainloop()
 
+id = 0
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer/trainer.yml')
 cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath);
 font = cv2.FONT_HERSHEY_SIMPLEX
-id = 0
 names = ['Unknow', 'Hugo', 'Flavien', 'Clement', 'Jeremy']
 url = 'http://' + str(ip) + ':8080/shot.jpg'
-
+connect = sqlite3.connect(':memory:')
+db = connect.cursor()
+db.execute('''CREATE TABLE students
+             (name, time)''')
 while True:
     try:
         imgNp=np.array(bytearray(urlopen(url).read()),dtype=np.uint8)
@@ -52,6 +57,7 @@ while True:
         id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
         if (confidence < 100):
             id = names[id]
+            db.execute("INSERT INTO students VALUES (?, ?);", (str(id), time.strftime("%A %d %B %Y %H:%M:%S")))
             confidence = "  {0}%".format(round(100 - confidence))
         else:
             id = "unknow"
@@ -59,4 +65,8 @@ while True:
         cv2.putText(image_frame, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
     cv2.imshow('face_recognition_students',image_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        connect.commit()
+        for row in db.execute('SELECT * FROM students'):
+            print (row)
+        connect.close()
         break

2d9dcdab766de46fa80842d71e545f949a22b0e4
Clem <clement.da-purificacao@epitech.eu>
2019-03-20 11:28:54 +0100

    UI from the project

--- ui.py
+++ ui.py
@@ -13,11 +13,11 @@ class UI():
         self.master = master
         self.master.title("Face recognition student")
         self.master.geometry("150x150")
-        self.label = tk.Label(master, text= 'Face recognition student')
-        self.detectionButton = tk.Button(master, text= 'Face detection', command= self.face_detection)
-        self.trainingButton = tk.Button(master, text= 'Training', command= self.train)
-        self.recognizeButton = tk.Button(master, text= 'Recognize', command= self.recognize)
-        self.exitButton = tk.Button(master, text= 'Exit', command= self.exit)
+        self.label = tk.Label(master, text='Face recognition student')
+        self.detectionButton = tk.Button(master, text='Face detection', command=self.face_detection)
+        self.trainingButton = tk.Button(master, text='Training', command=self.train)
+        self.recognizeButton = tk.Button(master, text='Recognize', command=self.recognize)
+        self.exitButton = tk.Button(master, text='Exit', command=self.exit)
         self.label.grid(columnspan=2)
         self.detectionButton.grid(column=0)
         self.trainingButton.grid(column=0)
@@ -42,18 +42,68 @@ class RecognizeWindow(UI):
 		self.g = tk.Toplevel()
 		self.g.title('Face recognition')
 		self.g.geometry("400x200")
-		self.menuButton = tk.Button(self.g, text= 'Main Menu', command= self.exitMenu)
+		self.menuButton = tk.Button(self.g, text='Main Menu', command=self.exitMenu)
 		self.menuButton.grid(column=0,row=0)
 		self.label_ip = tk.Label(self.g, text="What is the ip of the camera ?")
 		self.label_ip.grid(columnspan=1)
 		self.string_ip = tk.Entry(self.g)
 		self.string_ip.grid(columnspan=1)
 		self.string_ip.focus_set()
-		self.confirmButton = tk.Button(self.g, text= 'Confirm', command= self.confirm)
+		self.confirmButton = tk.Button(self.g, text='Confirm', command=self.confirm)
 		self.confirmButton.grid(column=0)
 
 	def exitMenu(self):
 		self.g.destroy()
+
+	def confirm(self):
+		string = self.string_ip.get()
+		self.ip = string
+		id = 0
+		recognizer = cv2.face.LBPHFaceRecognizer_create()
+		recognizer.read('trainer/trainer.yml')
+		cascadePath = "haarcascade_frontalface_default.xml"
+		faceCascade = cv2.CascadeClassifier(cascadePath);
+		font = cv2.FONT_HERSHEY_SIMPLEX
+		names = ['Unknow', 'Coco', 'Hugo']
+		url = 'http://' + str(self.ip) + ':8080/shot.jpg'
+		connect = sqlite3.connect(':memory:')
+		db = connect.cursor()
+		db.execute('''CREATE TABLE students
+		(name, time)''')
+		while True:
+			try:
+				imgNp=np.array(bytearray(urlopen(url).read()),dtype=np.uint8)
+			except Exception:
+				print('IP is not valid please retry')
+				break
+			image_frame=cv2.imdecode(imgNp,-1)
+			gray = cv2.cvtColor(image_frame,cv2.COLOR_BGR2GRAY)
+			faces = faceCascade.detectMultiScale(
+				gray,
+				scaleFactor = 1.2,
+				minNeighbors = 5,
+				minSize = (50, 50),
+				)
+			for(x,y,w,h) in faces:
+				cv2.rectangle(image_frame, (x,y), (x+w,y+h), (255,0,0), 2)
+				id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+				if (confidence < 100):
+					id = names[id]
+					db.execute("INSERT INTO students VALUES (?, ?);", (str(id), time.strftime("%A %d %B %Y %H:%M:%S")))
+					confidence = "  {0}%".format(round(100 - confidence))
+				else:
+					id = "unknow"
+					confidence = "  {0}%".format(round(100 - confidence))
+				cv2.putText(image_frame, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+			cv2.imshow('face_recognition_students',image_frame)
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				connect.commit()
+				for row in db.execute('SELECT * FROM students'):
+					print (row)
+				connect.close()
+				cv2.destroyWindow('face_recognition_students')
+				self.exitMenu()
+				break
 
 class DetectionWindow(UI):
 	def __init__(self, mainUI):
@@ -61,7 +111,7 @@ class DetectionWindow(UI):
 		self.g = tk.Toplevel()
 		self.g.title('Face detection')
 		self.g.geometry("400x200")
-		self.menuButton = tk.Button(self.g, text= 'Main Menu', command= self.exitMenu)
+		self.menuButton = tk.Button(self.g, text='Main Menu', command=self.exitMenu)
 		self.menuButton.grid(column=0,row=0)
 		self.label_id = tk.Label(self.g, text="Enter an id for the user (for example : 1, 2, 3)\nDo not take 1 for example if there is student.1.0.jpg\nChoose a number that is not part of the dataset folder :")
 		self.label_id.grid(columnspan=1)
@@ -72,7 +122,7 @@ class DetectionWindow(UI):
 		self.label_ip.grid(columnspan=1)
 		self.string_ip = tk.Entry(self.g)
 		self.string_ip.grid(columnspan=1)
-		self.confirmButton = tk.Button(self.g, text= 'Confirm', command= self.confirm)
+		self.confirmButton = tk.Button(self.g, text='Confirm', command=self.confirm)
 		self.confirmButton.grid(column=0)
 
 	def exitMenu(self):
@@ -89,11 +139,11 @@ class DetectionWindow(UI):
 		url = 'http://' + str(self.ip) + ':8080/shot.jpg'
 		while True:
 			try:
-				imgNp=np.array(bytearray(urlopen(url).read()),dtype=np.uint8)
+				imgNp = np.array(bytearray(urlopen(url).read()), dtype=np.uint8)
 			except Exception:
 				print('IP is not valid please retry')
 				break
-			image_frame=cv2.imdecode(imgNp,-1)
+			image_frame = cv2.imdecode(imgNp, -1)
 			gray = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
 			faces = frontal_face.detectMultiScale(
 				gray,
